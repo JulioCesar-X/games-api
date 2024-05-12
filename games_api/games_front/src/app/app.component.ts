8ecd2c079ef65500027e9d6862f882318eb7eccf
Júlio César Pereira <jc.imp10@gmail.com>
2024-05-12 17:29:43 +0100

    chore

--- games_api/games_front/src/app/app.component.ts
+++ games_api/games_front/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Test10Component } from './components/test10/test10.component';
+import { Test11Component } from './components/test11/test11.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Test10Component,
+    Test11Component
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- games_api/games_front/src/app/app.routes.ts
+++ games_api/games_front/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { GenreComponent } from './components/genre/genre.component';
+import { Test10Component } from './components/test10/test10.component';
 
 export const routes: Routes = [
 
   { path: 'genres', component: GenreComponent },
+  { path: 'test10', component: Test10Component }
 
 ];
